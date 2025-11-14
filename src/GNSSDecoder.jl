7791cd7b905cfa14c9892669237b990c3ce1cabe
siebc <226531417+siebc@users.noreply.github.com>
2025-11-14 14:11:57 +0100

    feat: export new reset_decoder_state function

--- src/GNSSDecoder.jl
+++ src/GNSSDecoder.jl
@@ -2,7 +2,7 @@ module GNSSDecoder
 
 using DocStringExtensions, GNSSSignals, BitIntegers, ViterbiDecoder, CRC
 
-export decode, GPSL1DecoderState, GalileoE1BDecoderState, is_sat_healthy, GNSSDecoderState
+export decode, GPSL1DecoderState, GalileoE1BDecoderState, is_sat_healthy, GNSSDecoderState, reset_decoder_state
 
 galCRC24 = crc(spec(24, 0x864cfb, 0x000000, false, false, 0x000000, 0xcde703))
 
