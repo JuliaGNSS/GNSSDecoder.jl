d75a1dbddf43ce8c78d3f45d6900fd4afd2ee581
siebc <226531417+siebc@users.noreply.github.com>
2026-05-08 18:45:28 +0200

    test: add test for GPS L1 subframe 4 and 5 decoding

--- test/gpsl1.jl
+++ test/gpsl1.jl
@@ -1,7 +1,19 @@
-BitIntegers.@define_integers 1536
+BitIntegers.@define_integers 4000
 BitIntegers.@define_integers 320
-const GPSL1DATA =
-    uint1536"0x8b010c06ef056f410d000004def4351756ed43ed2357f4afe163920d2f0bff00295b9ebf9f88b010c06ef035644808d518abf98cb9d2094bfe1c3e92dbb769706d42853f19a83172d0e0748b010c06ef014ecffa302d1c9d38430077d85c9473c27eef4e6ac5a0325b0050c0cedc3c47c8b010c06eeff39075aaaac5555556aaaaaaa55555556aaaaaaa55555556aaaaaaa55555559c8b010c06eefd21840aaaab2aaaaabcaaaaaaf2aaaaabcaaaaaaf2aaaaabcaaaaaaf2aaaaabc8b"
+
+const GPSL1DATA = [
+    uint4000"0x8baaa8beae7de8c0e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8beae7bd1c0100c0660003861bf59b8e80864cfbcccccc5010ca84df264bc510429fa48baaa8beae79c94064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8beae77b6479aaaaa5555556aaaaaaa55555556aaaaaaa55555556aaaaaaa55555559c8baaa8beae75aec500000a1f02d85ffffffffd7bc9904a35103943ee7972a0f43ab7fffff208baaa8beae73ee80e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8beae71d800100c0660003861bf59b8e80864cfbcccccc5010ca84df264bc510429fa48baaa8beae6fc74064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8beae6db307753509edab6e87b1abdfd6f2c2cb84d554e01250d045abb6b001394eab08baaa8beae6ba40510000960fd2789ffffff5a84366d2cdc02e9dc02f43b8b4b1557fffff208baaa8beae69ebc0e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8beae67d600100c0660003861bf59b8e80864cfbcccccc5010ca84df264bc510429fa48baaa8beae65ce8064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8beae63b54780502a400";
+    uint4000"0x0363bfb000597bb47cf7fffbe84159f0eaa123802f84aaaae48baaa8beae61adc520000c9f02d876000000a57bc992ddc3f44b58d5fcbd59d51bfffffffac8baaa8beae5fec40e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8beae5ddac0100c0660003861bf59b8e80864cfbcccccc5010ca84df264bc510429fa48baaa8beae5bcdc064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8beae59b987aaaaafaaaaaa95555555aaaaaaa95555555aaaaaaa95555555aaaaaaa308baaa8beae57aa4530000fe0fd27a00000000284366fb2ff3f47bc03b04ba9830d5ffffffac8baaa8beae55e580e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8beae53dc80100c0660003861bf59b8e80864cfbcccccc5010ca84df264bc510429fa48baaa8beae51c40064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8beae4fb787baaaacd55555435555550d55555435555550d55555435555550d55555108baaa8beae4daf05400007e0fd27a00000000284366fbde6fe89f29c092f53d5e088000008c8baaa8beae4bef40e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8beae49d9c0100c0660003861bf59b8e80864cfbccccc";
+    uint4000"0xc5010ca84df264bc510429fa48baaa8beae47ca0064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8beae45be479aaaaa5555556aaaaaaa55555556aaaaaaa55555556aaaaaaa55555559c8baaa8beae43a9455000049f02d876000000a57bc992d07616852504bee725d55928000008c8baaa8beae41e680e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8beae3fddc0100c0660003861bf59b8e80864cfbcccccc5010ca84df264bc510429fa48baaa8beae3dc54064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8beae3bbe87caaaa4d55555435555550d55555435555550d55555435555550d55555108baaa8beae39a60560000160fd2789ffffff5a84366d223fe926d292add278574958000008c8baaa8beae37e280e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8beae35d400100c0660003861bf59b8e80864cfbcccccc5010ca84df264bc510429fa48baaa8beae33c30064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8beae31b747daaaa7aaaaaa95555555aaaaaaa95555555aaaaaaa95555555aaaaaaa308baaa8beae2fa8057000021f02d85ffffffffd7bc99044d4f1329d5c2342408582a7fffff20";
+    uint4000"0x8baaa8beae2de7c0e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8beae2bdec0100c0660003861bf59b8e80864cfbcccccc5010ca84df264bc510429fa48baaa8beae29c64064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8beae27b947eaaaa25555556aaaaaaa55555556aaaaaaa55555556aaaaaaa55555559c8baaa8beae25a1c5800001e0fd27a00000000284366fb0625b42397c620ca2066007fffff208baaa8beae23e180e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8beae21d700100c0660003861bf59b8e80864cfbcccccc5010ca84df264bc510429fa48baaa8beae1fc1c064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8beae1db587f4444e11111127bbbbbb611111127bbbbbb61110600800000000003faac8baaa8beae1ba28737c3aafffffffffffffffffffffffffffffffffffffffffffffd5f81b448baaa8beae19ed40e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8beae17d080100c0660003861bf59b8e80864cfbcccccc5010ca84df264bc510429fa48baaa8beae15c80064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8beae13b3c79aaaaa555";
+    uint4000"0x5556aaaaaaa55555556aaaaaaa55555556aaaaaaa55555559c8baaa8beae11ab4410000e60fd2789ffffff5a84366d225edcce4c3e450be054ca6ffffffac8baaa8beae0fe340e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8beae0dd5c0100c0660003861bf59b8e80864cfbcccccc5010ca84df264bc510429fa48baaa8beae0bc2c064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8beae09b68593333460eae9066b60003284366fbbb0e45fcc85ce66da87fc28e5568388baaa8beae07a54420000b9f02d876000000a57bc992d08b38141ba5466bb610b4f000000008baaa8beae05ea80e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8beae03d380100c0660003861bf59b8e80864cfbcccccc5010ca84df264bc510429fa48baaa8beae01cb0064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8beadffb985a0000760fd2789ffffff5a84366d24f3c05009db4c31d15f1708000008c8baaa8beadfda104300008e0fd27a00000000284366fb376afe489109d15c4ac2bc000000008baaa8beadfbe140e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8beadf9d7c0100c0660003861bf59b8e80864cfbccccc";
+    uint4000"0xc5010ca84df264bc510429fa48baaa8beadf7c40064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8beadf5b045b000041f02d85ffffffffd7bc9904756325ad0d196e36e8d469ffffffac8baaa8beadf3a744400000e0fd27a00000000284366fb07f24c07b2dd92942841497fffff208baaa8beadf1e880e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8beadefd9c0100c0660003861bf59b8e80864cfbcccccc5010ca84df264bc510429fa48baaa8beadedc14064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8beadebba85c0000c1f02d85ffffffffd7bc9904a3a7f642b0bd25d7a022198000008c8baaa8beade9a2045000039f02d876000000a57bc992d61688c0f2cf334aad7ba53000000008baaa8beade7e680e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8beade5d000100c0660003861bf59b8e80864cfbcccccc5010ca84df264bc510429fa48baaa8beade3c70064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8beade1b3479aaaaa5555556aaaaaaa55555556aaaaaaa55555556aaaaaaa55555559c8baaa8beaddfa58460000660fd2789ffffff5a84366d2cb4c615cee6ef3ea234cee7fffff20";
+    uint4000"0x8baaa8beadddea40e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8beaddbd340100c0660003861bf59b8e80864cfbcccccc5010ca84df264bc510429fa48baaa8beadd9cbc064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8beadd7b4c5d0000f60fd2789ffffff5a84366d2ce1586d4daf213f23afb1d000000008baaa8beadd5ac447000051f02d85ffffffffd7bc990477c9494e2389001002f5757fffff208baaa8beadd3ec00e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8beadd1da80100c0660003861bf59b8e80864cfbcccccc5010ca84df264bc510429fa48baaa8beadcfc5c064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8beadcdb185e0000a9f02d876000000a57bc992d5f3d37f8e64fcd56867d257fffff208baaa8beadcba684800006e0fd27a00000000284366fb85d8d4f9e518ecf1b7c9e0ffffffac8baaa8beadc9e940e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8beadc7d480100c0660003861bf59b8e80864cfbcccccc5010ca84df264bc510429fa48baaa8beadc5cc0064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8beadc3b7c5f00009e0f";
+    uint4000"0xd27a00000000284366fb88525db4c29054efcd433a7fffff208baaa8beadc1af449000059f02d876000000a57bc992d89a21fcf1227270da0f7db000000008baaa8beadbfed40e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8beadbddbc0100c0660003861bf59b8e80864cfbcccccc5010ca84df264bc510429fa48baaa8beadbbccc064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8beadb9b8840aaaab2aaaaabcaaaaaaf2aaaaabcaaaaaaf2aaaaabcaaaaaaf2aaaaabc8baaa8beadb7ab44a0000060fd2789ffffff5a84366d2237670279fb570b92fe353000000008baaa8beadb5e480e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8beadb3dd80100c0660003861bf59b8e80864cfbcccccc5010ca84df264bc510429fa48baaa8beadb1c50064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8beadafb6879aaaaa5555556aaaaaaa55555556aaaaaaa55555556aaaaaaa55555559c8baaa8beadadae04b000031f02d85ffffffffd7bc9904ef5e76106f7c610fa2ff558000008c8baaa8beadabee40e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8beada9d8c0100c0660003861bf59b8e80864cfbccccc";
+    uint4000"0xc5010ca84df264bc510429fa48baaa8beada7cb0064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8beada5bf47eaaaa25555556aaaaaaa55555556aaaaaaa55555556aaaaaaa55555559c8baaa8beada3a844c0000b1f02d85ffffffffd7bc9904a2a212ac8475d9607982d7000000008baaa8beada1e780e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8bead9fdf40100c0660003861bf59b8e80864cfbcccccc5010ca84df264bc510429fa48baaa8bead9dc7c064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8bead9bbc074aaaaf2aaaaabcaaaaaaf2aaaaabcaaaaaaf2aaaaabcaaaaaaf2aaaaabc8baaa8bead99a484d0000860fd2789ffffff5a84366d24f51fb18f5784639a56b80000000008baaa8bead97e000e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8bead95d680100c0660003861bf59b8e80864cfbcccccc5010ca84df264bc510429fa48baaa8bead93c18064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8bead91b5c75aaaac5555556aaaaaaa55555556aaaaaaa55555556aaaaaaa55555559c8baaa8bead8faa84e0000d9f02d876000000a57bc992db1722e05562a3efaa9dd528000008c";
+    uint4000"0x8baaa8bead8de540e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8bead8bdc40100c0660003861bf59b8e80864cfbcccccc5010ca84df264bc510429fa48baaa8bead89c4c064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8bead87bbc76aaaa9aaaaaa95555555aaaaaaa95555555aaaaaaa95555555aaaaaaa308baaa8bead85a344f0000ee0fd27a00000000284366fbb370e85c78ed4199607d507fffff208baaa8bead83e300e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8bead81d580100c0660003861bf59b8e80864cfbcccccc5010ca84df264bc510429fa48baaa8bead7fc98064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8bead7dbdc79aaaaa5555556aaaaaaa55555556aaaaaaa55555556aaaaaaa55555559c8baaa8bead7baac500000a1f02d85ffffffffd7bc9904a35103943ee7972a0f43ab7fffff208baaa8bead79e500e9000f75555540aaaaaaf2aaaaabcaaaa0257fafbd4593dc273ffb9e8f48baaa8bead77d8c0100c0660003861bf59b8e80864cfbcccccc5010ca84df264bc510429fa48baaa8bead75c04064b410c9688d79f79cd40d52e77cdff5f32eba317a7ff29406c004370148baaa8bead73bb87753509eda";
+]
+
 
 @testset "GPS L1 constructor" begin
     gpsl1 = GPSL1()
@@ -104,80 +116,152 @@ end
 end
 
 @testset "GPS L1 test data decoding" begin
-    decoder = GPSL1DecoderState(1)
+    decoder = GPSL1DecoderState(25)
 
+    e_values       = zeros(31); e_values[25] = 0.006249904632568359
+    t_oa_values    = fill(0x000000000007c000, 31)
+    δi_values      = fill(0.017455023574651885, 31); δi_values[25] = 0.12939966841558787
+    Ω_dot_values   = zeros(31); Ω_dot_values[25] = 3.141616575226232e-7
+    sv_healths     = fill("00000000", 31)
+    sqrt_A_values  = fill(5153.70068359375, 31)
+    Ω_0_values     = [ 0.9309151162826294,-0.21355963682751117,-1.3601612153513243, -0.19503614470114108,
+                      -2.3907618634107664,-1.2934929722028254,  2.939997392795146,   2.9980856799254663,
+                       2.90515588043202,   0.8703842943137864, -0.40818644534672865,-2.2915688083761583,
+                       1.9468114948902273, 1.92800450094344,    1.8790392050223226, -2.2747965111140833,
+                      -1.2333033261367568, 0.8775070434848794, -1.1769426399279497,  0.8237562967562418,
+                      -0.1811449300077196, 0.8834358642497763,  1.897445476448837,  -0.15087727198112835,
+                       1.6921463411443713, 1.9447060163771674,  2.881117397807016,  -2.266460359171323,
+                      -1.2251210965826247,-2.3375200717335782,  2.9373462575418645]
+    ω_values       = [-1.2019776857552396, 2.714100745441627,   0.8899440473874415,  0.4732911216521067,
+                       1.2949861317246272,-1.463009084475038,   2.923551291154714,   2.976520067208277,
+                       1.459411195454233,  0.5908353955800706,  0.6840685457664337, -0.8127667625180299,
+                       1.5061837527246984,-2.0994983848320645, -0.7419976674097294, -0.38597817856898975,
+                      -2.7500264556516596,-2.4382575937528643, -0.39128456865286465, 1.3146076784579506,
+                      -2.6488990718159213,-1.82339682329378,    2.8824027059281154, -0.6395231817932232,
+                      -1.2294114490987422, 0.9676722320954713, -1.6511730252070136, -2.057136771340827,
+                      -1.3434334844256162, 1.4131749322482823, -1.1938287873266253]
+    M_0_values     = [ 0.7772687393343071, 1.8146314862965967,  1.4561147845912032,  -1.6325469181548915,
+                      -2.025272215345597, -2.302044518971553,  -0.004539025182903811,-0.6746275980902107,
+                      -0.9311753986672926, 2.675434018291522,  -0.14266433285067617,  0.18639551854437897,
+                       2.4951422152942526, 2.095625233145465,   2.600880155155299,   -2.332779561769453,
+                       2.86441326082409,  -2.204820247384678,   2.1227365459376077,  -2.0576318696322295,
+                      -0.9285714512995756, 2.953611472287631,   1.5835962280078775,  -2.3064959095098363,
+                      -0.9196229803655571, 1.1444365533932548, -2.7133509823709856,  -2.994534978790226,
+                      -0.8758738183344963,-2.562498053346696,  -0.07783042512675355]
+    af0_values     = zeros(31); af0_values[25] = 0.00043487548828125
+    af1_values     = zeros(31); af1_values[25] = 2.4920154828578234e-9
+    prns = [5; 16; 20; 12; 24; 28; 30; 17; 8; 1; 23; 22; 19; 6; 11; 31; 9; 14; 3; 29; 7; 25; 4; 13; 21; 2; 27; 18; 26; 10; 15]
+    test_almanac_data = Dict(
+        prn => (
+            e = e_values[prn],
+            t_oa = t_oa_values[prn],
+            δi = δi_values[prn],
+            Ω_dot = Ω_dot_values[prn],
+            sv_health = sv_healths[prn],
+            sqrt_A = sqrt_A_values[prn],
+            Ω_0 = Ω_0_values[prn],
+            ω = ω_values[prn],
+            M_0 = M_0_values[prn],
+            af0 = af0_values[prn],
+            af1 = af1_values[prn],
+        ) for prn in prns
+    )
     test_data = GNSSDecoder.GPSL1Data(;
-        last_subframe_id = 5,
+        last_subframe_id = 3,
         integrity_status_flag = false,
-        TOW = 34945 * 6,
+        TOW = 43333 * 6,
         alert_flag = false,
-        anti_spoof_flag = true,
-        trans_week = 67,
+        anti_spoof_flag = false,
+        trans_week = 58,
         codeonl2 = 1,
         ura = 2.0,
         svhealth = "000000",
-        IODC = "0001001000",
+        IODC = "0000000001",
         l2pcode = false,
-        T_GD = -1.0710209608078003e-8,
-        t_0c = 216000,
-        a_f2 = 0.0,
-        a_f1 = -4.774847184307873e-12,
-        a_f0 = -0.00018549291417002678,
-        IODE_Sub_2 = "01001000",
-        C_rs = 70.65625,
-        Δn = 3.930878022562108e-9,
-        M_0 = 2.4393048719362045,
-        C_uc = 3.604218363761902e-6,
-        e = 0.01144192845094949,
-        C_us = 1.3023614883422852e-5,
-        sqrt_A = 5153.7995529174805,
-        t_0e = 216000,
+        T_GD = 9.313225746154785e-10,
+        t_0c = 266400,
+        a_f2 = 2.9976021664879227e-15,
+        a_f1 = 1.0431904229335487e-9,
+        a_f0 = 8.355360478162766e-6,
+        IODE_Sub_2 = "00000001",
+        C_rs = 6,
+        Δn = 1.1702987476403108e-8,
+        M_0 = 0.7425089733705321,
+        C_uc = 1.000240445137024e-6,
+        e = 0.09999999997671694,
+        C_us = 2.000480890274048e-6,
+        sqrt_A = 5153.700811386108,
+        t_0e = 266400,
         fit_interval = false,
         AODO = 31,
-        C_ic = -1.73225998878479e-7,
-        Ω_0 = 0.0600607702978756,
-        C_is = -2.2351741790771484e-7,
-        i_0 = 0.9781895349147778,
-        C_rc = 136.34375,
-        ω = 0.635978551768012,
-        Ω_dot = -7.383521839035659e-9,
-        IODE_Sub_3 = "01001000",
-        i_dot = -3.4465721349922174e-10,
+        C_ic = 3.0007213354110718e-6,
+        Ω_0 = 1.6162756322990954,
+        C_is = 3.999099135398865e-6,
+        i_0 = 1.0717994640412103,
+        C_rc = 5,
+        ω = -1.2294114827458582,
+        Ω_dot = 3.1415915741848384e-7,
+        IODE_Sub_3 = "00000001",
+        i_dot = 3.142988060925545e-10,
+        α_0 = 4.6566128730773926e-9,
+        α_1 = 1.4901161193847656e-8,
+        α_2 = -5.960464477539063e-8,
+        α_3 = -5.960464477539063e-8,
+        β_0 = 79872.0,
+        β_1 = 65536.0,
+        β_2 = -65536.0,
+        β_3 = -393216.0,
+        A_0 = 0.00024970434606075287,
+        A_1 = 1.000000082740371e-9,
+        Δt_LS = 18,
+        t_ot = 507904,
+        WN_t = 58,
+        WN_LSF = 56,
+        DN = 2,
+        Δt_LSF = 18,
+        sv_config = [4*ones(31); 1],
+        sv_health_sf4_25 = [repeat(["000000"], 7); "111111"],
+        almanac = test_almanac_data,
+        sv_health_sf5_25 = repeat(["000000"], 24),
+        t_oa = 507904,
+        WN_a = 58,
     )
 
-    state = decode(decoder, GPSL1DATA, 1508)
-    @test state.data == test_data
+    state = reduce((dec, data) -> decode(dec, data, sizeof(data)*8), GPSL1DATA, init=decoder)
+    @test all(field -> getfield(state.data, field) == getfield(test_data, field), fieldnames(GNSSDecoder.GPSL1Data))
     @test is_sat_healthy(state) == true
 
-    state = decode(decoder, ~GPSL1DATA, 1508)
-    @test state.data == test_data
+    state = reduce((dec, data) -> decode(dec, ~data, sizeof(data)*8), GPSL1DATA, init=decoder)
+    @test all(field -> getfield(state.data, field) == getfield(test_data, field), fieldnames(GNSSDecoder.GPSL1Data))
     @test is_sat_healthy(state) == true
 
     # test confirm_data
+    max_vote = 1
+    state = GNSSDecoder.confirm_data(state, max_vote)
     state = GNSSDecoder.GNSSDecoderState(
         state;
         raw_data = GNSSDecoder.GPSL1Data(state.data, C_ic = state.data.C_ic+1)
     )
-    state = GNSSDecoder.confirm_data(state)
+    state = GNSSDecoder.confirm_data(state, max_vote)
     @test state.data.C_ic == test_data.C_ic # erroneous data not accepted
     state = GNSSDecoder.GNSSDecoderState(
         state;
         raw_data = GNSSDecoder.GPSL1Data(state.data, C_ic = state.data.C_ic+1)
     )
-    state = GNSSDecoder.confirm_data(state)
+    state = GNSSDecoder.confirm_data(state, max_vote)
     @test state.data.C_ic == test_data.C_ic # erroneous data not accepted
     state = GNSSDecoder.GNSSDecoderState(
         state;
         raw_data = GNSSDecoder.GPSL1Data(state.data, C_ic = state.data.C_ic+1)
     )
-    state = GNSSDecoder.confirm_data(state)
+    state = GNSSDecoder.confirm_data(state, max_vote)
     @test state.data.C_ic == test_data.C_ic+1 # erroneous data accepted as it has been provided more often than true data
 end
 
 @testset "confirm_data branches" begin
     # Setup: create a base state with valid data
     decoder = GPSL1DecoderState(1)
-    state = decode(decoder, GPSL1DATA, 1508)
+    state = reduce((dec, data) -> decode(dec, data, sizeof(data)*8), GPSL1DATA, init=decoder)
     base_data = state.data
 
     # Branch 1: New IODC (not in cache) and empty data - should use data immediately
@@ -323,7 +407,7 @@ end
 
 @testset "GPS L1 reset_decoder_state" begin
     decoder = GPSL1DecoderState(1)
-    state = decode(decoder, GPSL1DATA, 1508)
+    state = reduce((dec, data) -> decode(dec, data, sizeof(data)*8), GPSL1DATA, init=decoder)
 
     # test reset_decoder_state
     state = reset_decoder_state(state)
